a2db867685c1596851fa8c5e360ab66026fe9c20
James Davies <daviesj187@cardiff.ac.uk>
2024-12-14 23:20:58 +0000

    up

--- main.cpp
+++ main.cpp
@@ -3,6 +3,61 @@
 
 using namespace std;
 
+void handlePlayerInput(SDL_Event e, SDL_Rect *playerPtr)
+{
+
+    int speed = 10;
+    bool up = false;
+    bool down = false;
+    bool left = false;
+    bool right = false;
+
+    // keyStates works with continous presses
+    const Uint8 *keyStates = SDL_GetKeyboardState(NULL);
+
+    // Refactor this, does not work right when keys come up / down after pressing / holding
+    // Moving up or down
+    if (keyStates[SDL_SCANCODE_UP] ||
+        keyStates[SDL_SCANCODE_W] ||
+        keyStates[SDL_SCANCODE_DOWN] ||
+        keyStates[SDL_SCANCODE_S])
+    {
+        playerPtr->y += keyStates[SDL_SCANCODE_UP] ||
+                                keyStates[SDL_SCANCODE_W]
+                            ? -speed
+                            : speed;
+
+        // and left
+        if (keyStates[SDL_SCANCODE_LEFT] ||
+            keyStates[SDL_SCANCODE_A])
+            playerPtr->x -= speed;
+        // and right
+        else if (keyStates[SDL_SCANCODE_RIGHT] ||
+                 keyStates[SDL_SCANCODE_D])
+            playerPtr->x += speed;
+    }
+    // Moving left or right
+    else if (keyStates[SDL_SCANCODE_LEFT] ||
+             keyStates[SDL_SCANCODE_A] ||
+             keyStates[SDL_SCANCODE_RIGHT] ||
+             keyStates[SDL_SCANCODE_D])
+    {
+        playerPtr->x += keyStates[SDL_SCANCODE_LEFT] ||
+                                keyStates[SDL_SCANCODE_A]
+                            ? -speed
+                            : speed;
+
+        // and up
+        if (keyStates[SDL_SCANCODE_UP] ||
+            keyStates[SDL_SCANCODE_W])
+            playerPtr->y -= speed;
+        // and down
+        else if (keyStates[SDL_SCANCODE_DOWN] ||
+                 keyStates[SDL_SCANCODE_S])
+            playerPtr->y += speed;
+    }
+}
+
 void gameLoop(SDL_Window *window)
 {
     SDL_Surface *image = SDL_LoadBMP("./player.bmp");
@@ -23,15 +78,19 @@ void gameLoop(SDL_Window *window)
     cout << windowWidth << " " << windowHeight << endl;
 
     // the 50 is halfway across the rect and halfway down
-    SDL_Rect rect = {windowWidth / 2 - 50, windowHeight / 2 - 50, 100, 100};
+    SDL_Rect playerRect = {windowWidth / 2 - 50, windowHeight / 2 - 50, 100, 100};
+    SDL_Rect *playerPtr = &playerRect;
 
     while (!quit)
     {
         while (SDL_PollEvent(&e) != 0)
         {
-            if (e.type == SDL_QUIT)
+            switch (e.type)
             {
+            case SDL_QUIT:
                 quit = true;
+            case SDL_KEYDOWN:
+                handlePlayerInput(e, playerPtr);
             }
 
             // White
@@ -39,7 +98,7 @@ void gameLoop(SDL_Window *window)
 
             SDL_RenderClear(renderer);
 
-            SDL_RenderCopy(renderer, texture, NULL, &rect);
+            SDL_RenderCopy(renderer, texture, NULL, &playerRect);
 
             SDL_RenderPresent(renderer);
         }
@@ -50,8 +109,9 @@ void gameLoop(SDL_Window *window)
 }
 
 // GAME PLAN
-// GET A PLAYER SPRITE RENDERED ON THE SCREEN
 // HAVE GOOD PLAYER CONTROLS
+// HAVE CAMERA FOLLOW THE PLAYER AROUND WHEN MOVING
+// CREATE A VIEWPORT FOR THE PLAYER, HAVE THE VIEWPORT FOLLOW THE PLAYER SPRITE AROUND
 // HAVE THE WINDOW FOLLOW THE PLAYER SPRITE AROUND AS THEY MOVE
 
 int main()
